64587b66dd75419f89a5ffe488b15b59a2daa169
MyName <ivant4221@gmail.com>
2024-04-30 21:56:02 +0300

    now it works correct

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -8,30 +8,27 @@ print(f'Hello, {name}!')
 
 def main():
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    def quest(): 
-        result = 0   
+    correct = 0
+    while correct < 3:
         random_num = random.randint(1,50)
         print(f'Question: {random_num}')
         answer = prompt.string('Your answer: ')
 
         if random_num % 2 == 0 and answer == 'yes':
             print('Correct!')
-            result += 1
+            correct += 1
         elif random_num % 2 != 0 and answer == 'yes':
             print(f"'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, {name}")
         elif random_num % 2 != 0 and answer == 'no':
             print('Correct!')
-            result += 1
+            correct += 1
         elif random_num % 2 == 0 and answer == 'no':
             print(f"'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, {name}")
-        return result
-    counter = 0
-    if quest() > 0:
-        while counter < 2:
-            counter += 1
-            quest()
-    if counter == 2:
+            return correct
+    
+    if correct == 3:
         print(f'Congratulations, {name}!')
+
 
 if __name__ == 'main':
     main()
