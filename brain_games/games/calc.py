0977c77b691fb304d22091b125bbacfc12c3782e
MyName <ivant4221@gmail.com>
2024-05-28 20:00:05 +0300

    fix calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,7 +6,7 @@ def calc_logic():
     random_num2 = random.randint(1, 30)
     operations = ["+", "-", "*"]
     random_element = random.choice(operations)
-    question = f"Question:  {random_num1} {random_element} {random_num2}"
+    question = f"Question: {random_num1} {random_element} {random_num2}"
     answer = str(eval(f'{random_num1} {random_element} {random_num2}'))
     return question, answer
 
